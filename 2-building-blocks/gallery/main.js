a5e4304a930a4e5237c3b546a2327c454289f91f
tatamiya <tamyuji1002@gmail.com>
2021-09-16 13:32:29 +0900

    Image gallery: Adding an onclick handler to each thumbnail image

--- 2-building-blocks/gallery/main.js
+++ 2-building-blocks/gallery/main.js
@@ -6,7 +6,12 @@ const overlay = document.querySelector('.overlay');
 
 // Looping through the images
 for (let i = 1; i < 6; i++) {
+    const imageUrl = `images/pic${i}.jpg`
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    newImage.setAttribute('src', imageUrl);
     thumbBar.appendChild(newImage);
+
+    newImage.onclick = function () {
+        displayedImage.setAttribute('src', imageUrl)
+    }
 }
