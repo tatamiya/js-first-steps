92b3c7382686d5f489d202b0441862861b034b8e
tatamiya <tamyuji1002@gmail.com>
2021-09-16 13:23:01 +0900

    Image gallery: Looping through the images

--- 2-building-blocks/gallery/main.js
+++ 2-building-blocks/gallery/main.js
@@ -3,3 +3,10 @@ const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
+
+// Looping through the images
+for (let i = 1; i < 6; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    thumbBar.appendChild(newImage);
+}
