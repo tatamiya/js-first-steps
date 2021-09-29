72a4c4cb3b622a4345d512b34ac79f8c4f438092
tatamiya <tamyuji1002@gmail.com>
2021-09-29 23:04:36 +0900

    Drawing graphics: step 7~ of WebGB

--- 5-client-side-web-apis/drawing-graphics/webgl.js
+++ 5-client-side-web-apis/drawing-graphics/webgl.js
@@ -6,3 +6,37 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+let cube;
+
+let loader = new THREE.TextureLoader();
+
+loader.load('metal003.png', function (texture) {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(2, 2);
+
+    let geometry = new THREE.BoxGeometry(2.4, 2.4, 2.4);
+    let material = new THREE.MeshLambertMaterial({ map: texture, shading: THREE.FlatShading });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    draw();
+});
+
+let light = new THREE.AmbientLight('rgb(255, 255, 255)');
+scene.add(light)
+
+let spotLight = new THREE.SpotLight('rgb(255, 255, 255)');
+spotLight.position.set(100, 100, 100);
+spotLight.castShadow = true;
+scene.add(spotLight);
+
+function draw() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(draw);
+
+}
