4ae4ac29f61d6e9dd3cbc1004e59649b615fd6a8
tatamiya <tamyuji1002@gmail.com>
2021-10-01 22:59:36 +0900

    Getting started with React: Hello React

---
+++ intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App(props) {
+  console.log(props);
+  const subject = props.subject;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, {subject}!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
