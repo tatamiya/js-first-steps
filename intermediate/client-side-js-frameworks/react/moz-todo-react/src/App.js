40a617e5104e24d9071db4145cf987cf1c7f36ba
tatamiya <tamyuji1002@gmail.com>
2021-10-03 13:33:24 +0900

    Getting started with React: Componentizing our React app: Make a unique <Todo />

--- intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
+++ intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
@@ -46,9 +46,9 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo name="Eat" completed={true} id="todo-0" />
+        <Todo name="Sleep" completed={false} id="todo-1" />
+        <Todo name="Repeat" completed={false} id="todo-2" />
       </ul>
     </div>
   );
