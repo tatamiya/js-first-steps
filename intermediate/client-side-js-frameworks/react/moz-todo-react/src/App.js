245f8ae2fa9aea1c95821040660ed7ec7089a801
tatamiya <tamyuji1002@gmail.com>
2021-10-08 22:48:56 +0900

    Getting started with React: React interactivity: Adding a task

--- intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
+++ intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import React, { useState } from "react";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name) {
-    alert(name);
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks.map(task => (
+  const taskList = tasks.map(task => (
     <Todo
       id={task.id}
       name={task.name}
