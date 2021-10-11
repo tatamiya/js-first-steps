de627e00224b0f07f7a687a1a7f042355479a1cf
tatamiya <tamyuji1002@gmail.com>
2021-10-11 22:39:54 +0900

    Getting started with React: Rendering the filters

--- intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
+++ intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
@@ -4,8 +4,16 @@ import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function editTask(id, newName) {
     const editedTaskList = tasks.map(task => {
@@ -49,6 +57,10 @@ function App(props) {
     />
   ));
 
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton key={name} name={name} />
+  ));
+
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
@@ -56,9 +68,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {FileList}
       </div>
       <h2 id="list-heading">
         {headingText}
