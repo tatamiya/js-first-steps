399f7b5aeb21b0c7378cd048fc3bf06946ab9d0a
tatamiya <tamyuji1002@gmail.com>
2021-10-12 22:22:33 +0900

    Getting started with React: Focusing between templates

--- intermediate/client-side-js-frameworks/react/moz-todo-react/src/components/Todo.js
+++ intermediate/client-side-js-frameworks/react/moz-todo-react/src/components/Todo.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Todo(props) {
     const [isEditing, setEditing] = useState(false);
     const [newName, setNewName] = useState('');
+
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
 
     function handleChange(e) {
         setNewName(e.target.value)
@@ -27,6 +30,7 @@ export default function Todo(props) {
                     type="text"
                     value={newName}
                     onChange={handleChange}
+                    ref={editFieldRef}
                 />
             </div>
             <div className="btn-group">
@@ -63,7 +67,12 @@ export default function Todo(props) {
                 </label>
             </div>
             <div className="btn-group">
-                <button type="button" className="btn" onClick={() => setEditing(true)}>
+                <button
+                    type="button"
+                    className="btn"
+                    onClick={() => setEditing(true)}
+                    ref={editButtonRef}
+                >
                     Edit <span className="visually-hidden">{props.name}</span>
                 </button>
                 <button
@@ -76,5 +85,13 @@ export default function Todo(props) {
             </div>
         </div>
     );
+    useEffect(() => {
+        if (isEditing) {
+            editFieldRef.current.focus();
+        } else {
+            editButtonRef.current.focus();
+        }
+
+    }, [isEditing]);
     return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>
 }
