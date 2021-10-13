79170b7e0882cf8bcb3991cac492e209d337e348
tatamiya <tamyuji1002@gmail.com>
2021-10-13 22:54:51 +0900

    Getting started with React: Focusing when the user deletes a task

--- intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
+++ intermediate/client-side-js-frameworks/react/moz-todo-react/src/App.js
@@ -1,8 +1,16 @@
 import { nanoid } from "nanoid";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
+
+function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -70,6 +78,16 @@ function App(props) {
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -77,7 +95,7 @@ function App(props) {
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
-      <h2 id="list-heading">
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
         {headingText}
       </h2>
       <ul
