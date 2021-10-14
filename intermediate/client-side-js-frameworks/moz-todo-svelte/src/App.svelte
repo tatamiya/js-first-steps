eec1c7e3690d128308472d28be7fa05cb8015e27
tatamiya <tamyuji1002@gmail.com>
2021-10-14 23:24:12 +0900

    Getting started with Svelte: Making a couple of changes

--- intermediate/client-side-js-frameworks/moz-todo-svelte/src/App.svelte
+++ intermediate/client-side-js-frameworks/moz-todo-svelte/src/App.svelte
@@ -1,10 +1,22 @@
 <script>
 	export let name;
+
+	function toggleName() {
+		if (name === "world") {
+			name = "svelte";
+		} else {
+			name = "world";
+		}
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name} from MDN!</h1>
+	<botton on:click={toggleName}>Toggle name</botton>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
 </main>
 
 <style>
