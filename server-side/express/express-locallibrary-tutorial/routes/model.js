ebfc570bed51f289ae5d158b6a7086bd7b85085c
tatamiya <tamyuji1002@gmail.com>
2021-10-21 22:51:44 +0900

    Express Tutorial: Using models

--- server-side/express/express-locallibrary-tutorial/routes/model.js
+++ server-side/express/express-locallibrary-tutorial/routes/model.js
@@ -10,3 +10,20 @@ let SomeModelSchema = new Schema(
 );
 
 let SomeModel = mongoose.model('SomeModel', SomeModelSchema);
+
+let awesome_instance = new SomeModel({ name: 'awesome' });
+
+awesome_instance.save(function (err) {
+    if (err) return handleError(err);
+});
+
+SomeModel.create({ name: 'also_awesome' }, function (err, awsome_instance) {
+    if (err) return handleError(err);
+});
+
+console.log(awesome_instance.name);
+
+awesome_instance.name = "New cool name";
+awesome_instance.save(function (err) {
+    if (err) return handleError(err);
+});
