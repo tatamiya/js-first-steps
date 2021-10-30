d0e9770a6521878611aec04891ed0cbb6cd2e643
tatamiya <tamyuji1002@gmail.com>
2021-10-30 17:41:44 +0900

    Express Tutorial: Date formatting using luxon

--- server-side/express/express-locallibrary-tutorial/models/bookinstance.js
+++ server-side/express/express-locallibrary-tutorial/models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 var Schema = mongoose.Schema;
 
@@ -15,6 +16,12 @@ BookInstanceSchema
     .virtual('url')
     .get(function () {
         return '/catalog/bookinstance/' + this._id;
+    });
+
+BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get(function () {
+        return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
     });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
