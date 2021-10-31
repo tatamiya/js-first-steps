b12f8be04e67a31525e4ca87516a9a14f83692f1
tatamiya <tamyuji1002@gmail.com>
2021-10-31 12:21:47 +0900

    Express Tutorial: Genre list page

--- server-side/express/express-locallibrary-tutorial/controllers/genreController.js
+++ server-side/express/express-locallibrary-tutorial/controllers/genreController.js
@@ -1,7 +1,12 @@
 var Genre = require('../models/genre');
 
-exports.genre_list = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+exports.genre_list = function (req, res, next) {
+    Genre.find()
+        .sort([['name', 'ascending']])
+        .exec(function (err, list_genres) {
+            if (err) { return next(err); }
+            res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
+        });
 };
 
 exports.genre_detail = function (req, res) {
