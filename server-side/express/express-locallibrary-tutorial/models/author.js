26a6f3f190ae33ff29ea46a0d12d1763f78c96e5
tatamiya <tamyuji1002@gmail.com>
2021-10-31 12:17:40 +0900

    Express Tutorial: readable lifespan

--- server-side/express/express-locallibrary-tutorial/models/author.js
+++ server-side/express/express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 var Schema = mongoose.Schema;
 
@@ -33,6 +34,18 @@ AuthorSchema
     .virtual('url')
     .get(function () {
         return '/catalog/author/' + this._id;
+    });
+
+AuthorSchema
+    .virtual('date_of_birth_formatted')
+    .get(function () {
+        return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+    });
+
+AuthorSchema
+    .virtual('date_of_death_formatted')
+    .get(function () {
+        return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
     });
 
 module.exports = mongoose.model('Author', AuthorSchema);
