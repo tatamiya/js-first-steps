136176cf9ba918884c3b446b532ee7cb18739b56
tatamiya <tamyuji1002@gmail.com>
2021-11-03 17:53:16 +0900

    Express Tutorial: Create genre form

--- server-side/express/express-locallibrary-tutorial/controllers/genreController.js
+++ server-side/express/express-locallibrary-tutorial/controllers/genreController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 var Genre = require('../models/genre');
 var Book = require('../models/book');
 var async = require('async');
@@ -33,13 +34,44 @@ exports.genre_detail = function (req, res, next) {
     });
 };
 
-exports.genre_create_get = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+exports.genre_create_get = function (req, res, next) {
+    res.render('genre_form', { title: 'Create Genre' });
 };
 
-exports.genre_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+
+    body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var genre = new Genre(
+            { name: req.body.name }
+        );
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+            return;
+        }
+        else {
+            Genre.findOne({ 'name': req.body.name })
+                .exec(function (err, found_genre) {
+                    if (err) { return next(err); }
+
+                    if (found_genre) {
+                        res.redirect(found_genre.url);
+                    }
+                    else {
+                        genre.save(function (err) {
+                            if (err) { return next(err); }
+
+                            res.redirect(genre.url);
+                        });
+                    }
+                });
+        }
+    }
+];
 
 exports.genre_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Genre delete GET');
