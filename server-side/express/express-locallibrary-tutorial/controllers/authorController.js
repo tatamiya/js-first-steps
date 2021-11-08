bb86cc0919db0881f9ee928888d64459f62d91be
tatamiya <tamyuji1002@gmail.com>
2021-11-08 22:33:57 +0900

    Express Tutorial: Delete Author form (Controller—get route)

--- server-side/express/express-locallibrary-tutorial/controllers/authorController.js
+++ server-side/express/express-locallibrary-tutorial/controllers/authorController.js
@@ -70,8 +70,23 @@ exports.author_create_post =
         }
     ];
 
-exports.author_delete_get = function (req, res) {
-    res.send('NOT IMPLEMENTED: Author delete GET');
+exports.author_delete_get = function (req, res, next) {
+
+    async.parallel({
+        author: function (callback) {
+            Author.findById(req.params.id).exec(callback)
+        },
+        authors_books: function (callback) {
+            Book.find({ 'author': req.params.id }).exec(callback)
+        },
+    }, function (err, results) {
+        if (err) { return next(err); }
+        if (results.author == null) {
+            res.redirect('/catalog/authors');
+        }
+        res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books });
+    }
+    );
 };
 
 exports.author_delete_post = function (req, res) {
