f24d059400291551ba4ad364dae19d092f866ed8
tatamiya <tamyuji1002@gmail.com>
2021-11-16 23:17:12 +0900

    Express Tutorial: Update genre form (Post)

--- server-side/express/express-locallibrary-tutorial/controllers/genreController.js
+++ server-side/express/express-locallibrary-tutorial/controllers/genreController.js
@@ -130,6 +130,29 @@ exports.genre_update_get = function (req, res, next) {
     );
 };
 
-exports.genre_update_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+
+    body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var genre = new Genre(
+            {
+                name: req.body.name,
+                _id: req.params.id
+            }
+        );
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Update Genre', genre: genre, errors: errors.array() });
+            return;
+        }
+        else {
+            Genre.findByIdAndUpdate(req.params.id, genre, {}, function (err, thegenre) {
+                if (err) { return next(err); }
+                res.redirect(thegenre.url);
+            });
+        }
+    }
+];
