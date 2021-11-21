80ab7347eecba20118b5e3aeeed4b9c17af16022
tatamiya <tamyuji1002@gmail.com>
2021-11-21 12:11:03 +0900

    Express Tutorial: 7. Deploying to production (Use gzip/deflate compression for responses)

--- server-side/express/express-locallibrary-tutorial/app.js
+++ server-side/express/express-locallibrary-tutorial/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
 
 var app = express();
 
@@ -27,6 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
